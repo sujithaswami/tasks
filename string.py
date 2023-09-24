59170f9343ba852633c3be3479c06a3745b8d0c3
sujithaswami <144520487+sujithaswami@users.noreply.github.com>
2023-09-24 20:12:49 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,25 @@
+#string
+a ="suji"
+print(a)
+print(type(a))
+#explict
+a=str("10")
+print(a)
+print(type(a))
+#data annotation
+a:str= "sujitha"
+print(a)
+#str to bin
+a= bin(10)
+print(a)
+print(type(a))
+#str to bool
+a = str (True)
+print(a)
+print(type(a))
+s = str(0o1234)
+print(s)
+print(type(s))
+k = str(0xae45)
+print(k)
+print(type(a))
